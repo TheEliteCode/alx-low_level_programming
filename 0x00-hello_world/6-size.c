03563ff0f91ea8e481c858ffd62bd4513480eff1
TheEliteCode <davode12@gmail.com>
2022-10-13 11:23:36 -0700

    task 6 corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,22 @@
-/* 
- * file 6-size.c
- * a C program that prints the size of various types on the computer
- * based on the program it is run
+/*
+ * File: 6-size.c
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main  -prints the size of various types on the computer
- *         it is compiled to run
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
- *         Return (0)
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-   printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
