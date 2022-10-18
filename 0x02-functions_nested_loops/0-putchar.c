768da7ad7b6fd6155001895e558379c59b29c7aa
TheEliteCode <davode12@gmail.com>
2022-10-18 03:21:39 -0700

    A program that prints _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+/*
+ * File: 0-putchar.c
+ * Auth: TheEliteCode
+ *
+ */
+
+#include "stdio.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	putchar('_');
+
+	putchar('p');
+
+	putchar('u');
+
+	putchar('t');
+
+	putchar('c');
+
+	putchar('h');
+
+	putchar('a');
+
+	putchar('r');
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A list of tasks that deal with the topic, functions and nested loops 
