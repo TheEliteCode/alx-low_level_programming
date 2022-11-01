7a865ab4eae207964f12dbee5c7e2639e1c8ea5a
TheEliteCode <davode12@gmail.com>
2022-10-31 17:58:20 -0700

    Alx pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Set the value of a pointer to a char.
+ * @s: pointer to the char pointer to change
+ * @to: pointer to change the char pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
