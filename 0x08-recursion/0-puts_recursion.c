44283dcc76f5c6f445d2ed5c0e038e758d88273c
TheEliteCode <davode12@gmail.com>
2022-11-02 17:46:49 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string followed by a newline
+ * @s: a pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
