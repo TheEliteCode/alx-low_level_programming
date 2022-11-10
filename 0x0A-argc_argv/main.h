7b6e4728856151c7c39b29d8e6bcdab0965aed8b
TheEliteCode <davode12@gmail.com>
2022-11-09 21:20:14 -0800

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main (int argc, char *argv[]);
+int _putchar(char);
+void _puts(char *);
+void print_number(int);
+int _atoi(char *);
+int isnumber(char *);
+int subtract_largest_coin(int);
+
+#endif
